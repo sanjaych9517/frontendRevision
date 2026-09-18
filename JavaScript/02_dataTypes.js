530045be32ffeadacfe05589ac784e1a7c51f146
unknown <sanjaych9517@gmail.com>
2026-09-18 12:14:12 +0530

    learn sata types

---
+++ JavaScript/02_dataTypes.js
@@ -0,0 +1,24 @@
+// Data Types
+// A data types specifies what kind of value a variable contains.
+// types of data types:
+// 1. premetive data types
+// i. string: string represents textual data, collection of characters(alphabet,special character,alphanumeric) "" '' 
+// ii. number: interger , float 
+// iii. boolean: true/false - use when you want to check some condition
+// iv. BigInt: very large integer that cannot be safely represented by normal Number type
+// 124576768768782137612368712638611237637687n.
+// v. undefined: when you declared variable,but didn't assign value.
+// vi. null: intentionally empty. = null;
+// 2. Non-premetive/complex data types
+// i. array: 
+// ii. object:
+// iii. function:
+// iv: JSON
+
+// how to check data types
+// typeof
+// console.log(typeof 12345)
+// console.log(typeof 12.345)
+// console.log(typeof "Helloo")
+// console.log(typeof true)
+// console.log(typeof null)
---
+++ JavaScript/04_operator.js
@@ -0,0 +1,15 @@
+//  operaters -> operators are a symbol are keyword used to perform an operation  on one or more values
+
+// types of operator
+// Arithmetic operator -> Used to perform arithmetic operation
+//  + - * / %
+
+// console.log(10 + 20);
+
+// sum of two number
+
+// const num1 = 20
+// const num2 = 28
+// const num3 = 56
+
+// let sum = num1 + num2 + num3
