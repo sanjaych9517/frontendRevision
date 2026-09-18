706dc9cb1b99238a59910a74b1277bdbff8afb1d
unknown <sanjaych9517@gmail.com>
2026-09-18 12:26:33 +0530

    we learn if else in js

---
+++ JavaScript/05_ifElse.js
@@ -0,0 +1,39 @@
+// Conditional Statements in Js: block of code will execute while certain condition is met
+// if(condition){ // if true
+// // block of code to be executed}
+// else{
+// remaining part to be executed}
+
+// Number is Positive
+
+// let num = 0;
+// if (num > 0) {
+//     alert("Number is Positive")
+// }
+// else if (num < 0) {
+//     alert("Number is negative")
+// }
+// else {
+//     alert("Number is 0")
+// }
+
+// Alert "Login Successful" if username is "admin",password is "admin123" or show "Wrong Credential"
+
+// let userName = "admin1";
+// if (userName === "admin") {
+//     console.log("Login Successful")
+// } else {
+//     console.log("Wrong Credential")
+// }
+
+// let userName = "admin"
+// let password = "admin123";
+// if (userName === "admin" && password === "admin123") {
+//     alert("Login Successful")
+// } else {
+//     alert("Wrong Credential")
+// }
+
+// Alert "Login Successful" if username is "admin" or "user",
+
+// num % 2 === 0 - even
