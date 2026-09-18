cee392aa1852c438ca516fb1cf79ad6c9705d90a
unknown <sanjaych9517@gmail.com>
2026-09-18 12:22:25 +0530

    we learn operato in js

--- JavaScript/04_operator.js
+++ JavaScript/04_operator.js
@@ -1,15 +1,62 @@
-//  operaters -> operators are a symbol are keyword used to perform an operation  on one or more values
+// Operators in JS: Operators are a symbol or keyword used to perform an operation on one or more value.
+// Types of Operators:
+// Arithmetic Operator: used for arithmetic calculations
+// + - * / %
+// console.log(10 + 20)
+// // sum of two numbers
+// const num1 = 40;
+// const num2 = 50;
+// const num3 = 10;
+// let sum = num1 + num2 + num3;
+// console.log(sum) // 100 // sum will be 100
 
-// types of operator
-// Arithmetic operator -> Used to perform arithmetic operation
-//  + - * / %
+// concatination: used to merge variable or dynamic value and string. +
+// let userName = "John";
+// let age = 26;
+// // My name is John,My age is 26
+// console.log("My name is " + userName + " ,My age is " + age)
 
-// console.log(10 + 20);
+// user-input - prompt()
+// let num1 = parseInt(prompt("Enter a number")); // "10"
+// let num2 = parseInt(prompt("Enter another number")); // "20"
+// console.log(num1 + num2) // 1020
 
-// sum of two number
+// 9%2 = 1
 
-// const num1 = 20
-// const num2 = 28
-// const num3 = 56
+// 2. Assignment operator: used to assign value
+// = += -= *= /= %=
 
-// let sum = num1 + num2 + num3
+// num *= 1
+// num = num * 1
+
+// sum += num
+// sum = sum + num
+
+// 3. Comparison Operators: used to compare two or more values
+// > < >=(greater than or equals to) <= == === != !==
+// console.log(10 > 2)
+// console.log(10 < 2)
+// console.log(10 >= 20)
+// console.log(10 <= 20)
+// console.log(50 >= 20)
+// console.log(30 == 30)
+// difference b/w = == ===
+// = value will assign
+// == value will compare
+// === value & data type both will compare
+// console.log(5 == 5)
+// console.log(5 == "5")
+// console.log(5 == "4")
+// console.log(5 === 5)
+// console.log(5 === "5")
+// console.log(5 !== "5")
+// console.log(5 != 4)
+
+
+// logical operators: && (and) || (or) !(not) - if else
+// used in conditional statements for checking multiple conditions
+
+console.log(10 > 20 && 40 > 30) // f
+console.log(10 > 20 || 40 > 30) // t
+
+// ternary operator: alternate to if else
